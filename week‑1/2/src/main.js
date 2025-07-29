adf779f00f35a152aa2b4c5671cfbf875f8d1750
Nina <ninakya2003@gmail.com>
2025-07-29 13:48:54 +0300

    feat(week1-task2): add and integrate hash table

--- week‑1/2/src/main.js
+++ week‑1/2/src/main.js
@@ -1,10 +1,13 @@
-import { stackDemo, queueDemo, hanoiTowersDemo } from './demos/index.js';
+import { stackDemo, queueDemo, hashTableDemo, hanoiTowersDemo } from './demos/index.js';
 
-  console.log('Демонстрация стека');
-  stackDemo();
-  
-  console.log(' Демонстрация очереди');
-  queueDemo();
-  
-  console.log('Ханойские башни');
-  hanoiTowersDemo();
+console.log('Демонстрация стека');
+stackDemo();
+
+console.log(' Демонстрация очереди');
+queueDemo();
+
+console.log(' Демонстрация хэш таблицы');
+hashTableDemo();
+
+console.log('Ханойские башни');
+hanoiTowersDemo();
