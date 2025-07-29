7fbf0846260da7e98682320626e357541a621e8a
Nina Kniazeva <101893560+Goldenframe@users.noreply.github.com>
2025-07-29 11:46:01 +0300

    Create main.js

---
+++ week‑1/2/src/main.js
@@ -0,0 +1,13 @@
+import { stackDemo } from './demos/stack-demo.js';
+import { queueDemo } from './demos/queue-demo.js';
+import { hanoiTowersDemo } from './demos/hanoi-tower-demo.js';
+
+
+  console.log('Демонстрация стека');
+  stackDemo();
+  
+  console.log(' Демонстрация очереди');
+  queueDemo();
+  
+  console.log('Ханойские башни');
+  hanoiTowersDemo();
