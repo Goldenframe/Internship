5fee593379e6fba5f894bd67abe8255b3a80b441
Nina <ninakya@mail.ru>
2025-07-30 18:55:55 +0300

    fix(week1-task3): fix inner loop from going out of range

--- week‑1/3/src/algorithms/bubble-sort.js
+++ week‑1/3/src/algorithms/bubble-sort.js
@@ -1,7 +1,7 @@
 export function bubbleSort(arr) {
     const arrCopy = [...arr];
     for (let i = 0; i <= arrCopy.length - 1; i++) {
-        for (let j = 0; j <= arrCopy.length - 1 - i; j++) {
+        for (let j = 0; j < arrCopy.length - 1 - i; j++) {
             if (arrCopy[j] >= arrCopy[j + 1]) {
                 let temp = arrCopy[j];
                 arrCopy[j] = arrCopy[j + 1];
