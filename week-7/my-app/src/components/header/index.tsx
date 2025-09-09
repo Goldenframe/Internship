4c4ec54fd374c6d53998cd80b59ba3d3bd40cfdc
Nina <ninakya@mail.ru>
2025-09-09 13:19:50 +0300

    feat(week7-task1): add header

---
+++ week-7/my-app/src/components/header/index.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import styles from "./styles.module.css";
+
+export const Header = () => {
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.menu}>
+        <li className={styles.item}>
+          <Link href="/" className={styles.link}>
+            Главная
+          </Link>
+        </li>
+        <li className={styles.item}>
+          <Link href="/favorites" className={styles.link}>
+            Избранное
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
